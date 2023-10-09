c686496ee7a20da7814a2bf950b4f83fbf62ad9b
Steven <steve.theron365@gmail.com>
2023-10-09 19:52:25 +0200

    Added a Bullets class to use as a template for creating bullet entity's for the Player and Aliens.

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# Tutorial by Coding With Russ
+# https://www.youtube.com/watch?v=mqz1_wRSMwo&ab_channel=CodingWithRuss
+# Game created by Steven Theron
+# 9 October 2023
+
 import pygame
 from pygame.locals import *
 
@@ -18,11 +23,11 @@ bg = pygame.image.load("assets/background.png")
 red = (255, 0, 0)
 green = (0, 255, 0)
 
+
 def draw_bg():
     screen.blit(bg, (0, 0))
 
 
-# Create spaceship class
 class Spaceship(pygame.sprite.Sprite):
     def __init__(self, x, y, health):
         pygame.sprite.Sprite.__init__(self) # inherit Sprite class within the Spaceship class.
@@ -31,9 +36,11 @@ class Spaceship(pygame.sprite.Sprite):
         self.rect.center = [x, y]
         self.health_start = health
         self.health_remaining = health
+        self.last_shot = pygame.time.get_ticks()  # Measures when bullet was created.
 
     def update(self):
         speed = 6
+        cooldown = 500  # milliseconds.
 
         # Get key pressed
         key = pygame.key.get_pressed()
@@ -41,6 +48,15 @@ class Spaceship(pygame.sprite.Sprite):
             self.rect.x -= speed
         if key[pygame.K_RIGHT] and self.rect.right < SCREEN_WIDTH:
             self.rect.x += speed
+
+        # Record current time
+        time_now = pygame.time.get_ticks()
+
+        # Shoot
+        if key[pygame.K_SPACE] and (time_now - self.last_shot > cooldown):
+            bullet = Bullets(self.rect.centerx, self.rect.top)
+            bullet_group.add(bullet)
+            self.last_shot = time_now
 
         # Draw Health bar
         pygame.draw.rect(screen, red, (self.rect.x, (self.rect.bottom + 10), self.rect.width, 15))
@@ -50,8 +66,22 @@ class Spaceship(pygame.sprite.Sprite):
                                              int(self.rect.width * (self.health_remaining / self.health_start)), 15))
 
 
+class Bullets(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)  # inherit Sprite class within the Spaceship class.
+        self.image = pygame.image.load("assets/bullet.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
+    def update(self):
+        self.rect.y -= 5
+        if self.rect.bottom < 0:
+            self.kill()  # garbage collection for when bullets leave the screen.
+
+
 # Create sprite group for tracking list of activate sprites
 spaceship_group = pygame.sprite.Group()
+bullet_group = pygame.sprite.Group()
 
 # Create Player
 spaceship = Spaceship(int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT - 100), 3)
@@ -73,8 +103,12 @@ while run:
     # Update spaceship
     spaceship.update()
 
+    # Update sprite Groups.
+    bullet_group.update()
+
     # Draw Sprite Groups
     spaceship_group.draw(screen)
+    bullet_group.draw(screen)
 
     pygame.display.update()
 
